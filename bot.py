d1e1161a1f8a71ae89f7eb0c67b857c33957e45e
Vlad Budilin <70877288+vabudilin@users.noreply.github.com>
2025-08-22 17:57:13 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,30 +3,38 @@ import random
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# Получаем токен из переменной окружения
+   # Получаем токен из переменной окружения
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-async def boobs(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Обработчик команды /thing@BotName"""
-    user = update.effective_user
-    random_length = random.randint(80, 150)  # Случайное число от 1 до 100
-    response = f"@{user.username}, ваша грудь {random_length} сантиметров в обхвате"
-    await update.message.reply_text(response)
+async def thing(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+   """Обработчик команды /thing@BotName"""
+   user = update.effective_user
+   if not user.username:
+       response = f"{user.first_name}, ваша грудь {random.randint(75, 150)} сантиметров в обхвате"
+   else:
+       response = f"@{user.username}, ваша грудь {random.randint(75, 150)} сантиметров в обхвате"
+   await update.message.reply_text(response)
 
-def main() -> None:
+async def main() -> None:
     """Запуск бота"""
+    if not TOKEN:
+       raise ValueError("TELEGRAM_BOT_TOKEN не установлен в переменных окружения")
+
     application = Application.builder().token(TOKEN).build()
 
     # Регистрируем обработчик команды
-    application.add_handler(CommandHandler("thing", boobs))
+    application.add_handler(CommandHandler("thing", thing))
 
     # Запускаем бота в режиме webhook
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.getenv("PORT", 8443)),
-        url_path=TOKEN,
-        webhook_url=f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}"
+    port = int(os.getenv("PORT", 8443))
+    webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}"
+    await application.bot.set_webhook(webhook_url)
+    await application.run_webhook(
+       listen="0.0.0.0",
+       port=port,
+       url_path=TOKEN
     )
 
 if __name__ == "__main__":
-    main()
+    import asyncio
+    asyncio.run(main())
